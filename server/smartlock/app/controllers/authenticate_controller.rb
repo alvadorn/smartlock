ea60e7c4d7e6ebd4c7c8dd0561ce96d370482ea5
Igor Santana <igorba2005@gmail.com>
2015-11-04 20:48:15 -0200

    Controller performance

--- server/smartlock/app/controllers/authenticate_controller.rb
+++ server/smartlock/app/controllers/authenticate_controller.rb
@@ -1,37 +1,40 @@
 class AuthenticateController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def init
   	account = params[:account]
   	password = params[:password]
   	bt = params[:bt]
   	token = params[:token] if bt == "true"
   	pass_hash = password
-  	user = User.find_by(account: account, password: pass_hash)
-  	if user
-  		if bt == "true"
-  			bluetooth = BluetoothAdapter.find_by(token: token, user_id: user.id)
-  			if bluetooth
-  				if bluetooth.activated
-  					authorized user.name
-  				else
-  					unauthorized
-  				end
-  			else
-  				unauthorized
-  			end
-  		else
-  			authorized user.name
-  		end
-  	end
+    if bt == "true"
+      query = User.joins(:bluetooth_adapter).where( bluetooth_adapters: {
+        token: token }, account: account, password: pass_hash)
+      if query.any?
+        authorized query[0].name
+      else
+        unauthorized
+      end
+    else
+      user = User.find_by(account: account, password: pass_hash)
+      if user
+        authorized user.name
+      else
+        unauthorized
+      end
+    end
   end
 
 
   private
   def unauthorized
-  	render json: "{ \"authentication\": \"unauthorized\" }"
+  	render json: { authentication: "unauthorized" }
+    #{}"{ \"authentication\": \"unauthorized\" }"
   end
 
   def authorized(name)
-  	render json: "{ \"authentication\": \"authorized\", \"name\": #{name} }"
+  	render json:  { authentication: "authorized", name: name }
+    #{}"{ \"authentication\": \"authorized\", \"name\": #{name} }"
   end
+
 end
