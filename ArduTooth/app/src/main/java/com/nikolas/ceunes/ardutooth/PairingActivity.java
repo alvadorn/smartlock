84a88de9e56167499fcd032c47b423d705f70eba
Nikolas <nikolas.serafini@gmail.com>
2015-11-19 10:56:34 -0200

    Showing acess token on screen!

--- ArduTooth/app/src/main/java/com/nikolas/ceunes/ardutooth/PairingActivity.java
+++ ArduTooth/app/src/main/java/com/nikolas/ceunes/ardutooth/PairingActivity.java
@@ -1,5 +1,6 @@
 package com.nikolas.ceunes.ardutooth;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -9,7 +10,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
@@ -18,6 +18,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
@@ -33,9 +34,9 @@ import java.util.Date;
 import java.util.Set;
 import java.util.UUID;
 
-public class PairingActivity extends AppCompatActivity {
+public class PairingActivity extends Activity {
 
-    Button pairingButton, turnOnButton, turnOffButton, visibleButton, inputButton;
+    Button pairingButton, turnOnButton, turnOffButton, visibleButton, inputButton, instantButton;
 
     private BluetoothAdapter adapter;
     private Set<BluetoothDevice> pairedDevices;
@@ -46,6 +47,7 @@ public class PairingActivity extends AppCompatActivity {
     private String token;
     private EditText loginInput;
     private EditText passwordInput;
+    private TextView showToken;
 
     SharedPreferences prefs = null;
     DatabaseHelper database;
@@ -60,14 +62,20 @@ public class PairingActivity extends AppCompatActivity {
         turnOffButton = (Button) findViewById(R.id.button3);
         visibleButton = (Button) findViewById(R.id.button4);
         inputButton = (Button) findViewById(R.id.button5);
+        instantButton = (Button) findViewById(R.id.button6);
         loginInput = (EditText) findViewById(R.id.editText);
         passwordInput = (EditText) findViewById(R.id.editText2);
-
+        showToken = (TextView) findViewById(R.id.textView);
 
         adapter = BluetoothAdapter.getDefaultAdapter();
         prefs = getSharedPreferences("com.nikolas.ceunes.ardutooth", MODE_PRIVATE);
         token = new String();
         database = new DatabaseHelper(this);
+
+        Cursor c = database.getAllEntries();
+        c.moveToFirst();
+        showToken.setText("\t\t\t\t\t\t YOUR TOKEN" + "\n" + c.getString(c.getColumnIndex("token")));
+
         inputStream = new InputStream() {
             @Override
             public int read() throws IOException {
@@ -187,6 +195,39 @@ public class PairingActivity extends AppCompatActivity {
     }
 
     public void sendData() throws IOException, JSONException {
+        JSONObject bj = new JSONObject();
+        Cursor c = database.getAllEntries();
+
+        try {
+            c.moveToFirst();
+            if (c == null) {
+                database.insertData(loginInput.getText().toString(), passwordInput.getText().toString(), token);
+                bj.put("account", loginInput.getText().toString());
+                bj.put("password", passwordInput.getText().toString());
+                bj.put("token", token);
+                Log.d("Mensagem!", "Enviando dados do input!");
+                Log.d("Login", loginInput.getText().toString());
+                Log.d("Password", passwordInput.getText().toString());
+                Log.d("Token", token);
+            } else {
+                bj.put("account", c.getString(c.getColumnIndex("login")));
+                bj.put("password", c.getString(c.getColumnIndex("password")));
+                bj.put("token", c.getString(c.getColumnIndex("token")));
+                Log.d("Mensagem!", "Enviando do banco de dados!");
+                Log.d("Login", c.getString(c.getColumnIndex("login")));
+                Log.d("Password", c.getString(c.getColumnIndex("password")));
+                Log.d("Token", c.getString(c.getColumnIndex("token")));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        outputStream.write(bj.toString().getBytes());
+        Toast.makeText(getApplicationContext(), "Credentials sent sucessfully!", Toast.LENGTH_LONG).show();
+        c.close();
+        closeConnection();
+    }
+
+    public void instantAuth(View v) throws IOException, JSONException {
         JSONObject bj = new JSONObject();
         Cursor c = database.getAllEntries();
 
